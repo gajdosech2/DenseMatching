38f75f223751c911e90668849c99ba51289333b1
gajdosech2 <l.gajdosech@gmail.com>
2023-01-20 14:27:28 +0100

    fixed type errors, added pyinstaller command

--- demo.py
+++ demo.py
@@ -134,8 +134,8 @@ if __name__ == "__main__":
     elif len(sys.argv) == 3:
         main(sys.argv[1], sys.argv[2])     
     elif len(sys.argv) == 4:
-        main(sys.argv[1], sys.argv[2], sys.argv[3])      
+        main(sys.argv[1], sys.argv[2], int(sys.argv[3]))      
     elif len(sys.argv) == 5:
-        main(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])  
+        main(sys.argv[1], sys.argv[2], int(sys.argv[3]), int(sys.argv[4]))  
     elif len(sys.argv) == 6:
-        main(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5])  
+        main(sys.argv[1], sys.argv[2], int(sys.argv[3]), int(sys.argv[4]), float(sys.argv[5]))  
